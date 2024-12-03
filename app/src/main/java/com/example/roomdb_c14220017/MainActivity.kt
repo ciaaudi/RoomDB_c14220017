ee147427e73c85d844f1a408b4ea1e846138316e
ciaaudi <146406439+ciaaudi@users.noreply.github.com>
2024-12-03 20:24:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/roomdb_c14220017/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.roomdb_c14220017
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomdb_c14220017.database.daftarBelanja
+import com.example.roomdb_c14220017.database.daftarBelanjaDB
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var DB : daftarBelanjaDB
+    private lateinit var adapterDaftar: adapterDaftar
+
+    private var arDaftar : MutableList<daftarBelanja> = mutableListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        adapterDaftar = adapterDaftar(arDaftar)
+        var _rvDaftar = findViewById<RecyclerView>(R.id.rvDaftar)
+
+        _rvDaftar.layoutManager = LinearLayoutManager(this)
+        _rvDaftar.adapter = adapterDaftar
+
+        DB = daftarBelanjaDB.getDatabase(this)
+
+        var _fabAdd = findViewById<FloatingActionButton>(R.id.fabAdd)
+        _fabAdd.setOnClickListener {
+            startActivity(Intent(this, TambahDaftar::class.java))
+        }
+
+        super.onStart()
+        CoroutineScope(Dispatchers.Main).async {
+            val daftarBelanja = DB.fundaftarBelanjaDAO().selectAll()
+            Log.d("data ROOM", daftarBelanja.toString())
+            adapterDaftar.isiData(daftarBelanja)
+        }
+
+        adapterDaftar.setOnItemClickCallBack(
+            object : adapterDaftar.OnItemClickCallBack{
+                override fun delData(dtBelanja: daftarBelanja) {
+                    CoroutineScope(Dispatchers.IO).async {
+                        DB.fundaftarBelanjaDAO().delete(dtBelanja)
+                        val daftar = DB.fundaftarBelanjaDAO().selectAll()
+                        withContext(Dispatchers.Main){
+                            adapterDaftar.isiData(daftar)
+                        }
+                    }
+                }
+            }
+        )
+    }
+}
---
+++ app/src/main/java/com/example/roomdb_c14220017/database/daftarBelanjaDAO.kt
@@ -0,0 +1,26 @@
+package com.example.roomdb_c14220017.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface daftarBelanjaDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(daftar: daftarBelanja)
+
+    @Query("UPDATE daftarBelanja SET tanggal=:isi_tanggal, item=:isi_item, jumlah=:isi_jumlah, status=:isi_status " +
+            "WHERE id=:pilihid")
+    fun update(isi_tanggal: String, isi_item: String, isi_jumlah: String, isi_status: Int, pilihid: Int)
+
+    @Delete
+    fun delete(daftar: daftarBelanja)
+
+    @Query("SELECT * FROM daftarBelanja ORDER BY id asc")
+    fun selectAll() : MutableList<daftarBelanja>
+
+    @Query("SELECT * FROM daftarBelanja WHERE id=:isi_id")
+    suspend fun getItem(isi_id: Int) : daftarBelanja
+}
